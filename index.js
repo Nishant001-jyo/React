c695bf45dd570542b9a4a22f77621a7657fed069
Nishant Kumar <nishantsaw001@gmail.com>
2026-09-02 20:20:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// var h1 = document.createElement("h1")
+// h1.innerHTML ="hello from js"
+
+// document.body.appendChild(h1)
+
+import {arr} from './app.js'
+console.log(arr)
+import user from './app.js'
+console.log(user)
---
+++ script.jsx
@@ -0,0 +1,1 @@
+var h1= <h1>hello </h1>
